faf4a013ecbb4f2e5cf0115d938f12097f1399cc
Bartosz-Kacprzak <s10731bk@ms.wwsi.edu.pl>
2025-03-11 22:39:18 +0100

    Operatory_zad1

--- jipp/jipp/Program.cs
+++ jipp/jipp/Program.cs
@@ -1,1 +1,13 @@
-﻿int result = 5 + 9; 
+﻿int number = 22;
+float money = 3.2F;
+string text = "git";
+bool isLogged = true;
+char myChar = 'g';
+decimal price = 3.21M;
+Console.WriteLine(number);
+Console.WriteLine(money);
+Console.WriteLine(text);
+Console.WriteLine(isLogged);
+Console.WriteLine(myChar);
+Console.WriteLine(price);
+
