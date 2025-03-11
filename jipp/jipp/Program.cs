040425d2320c8cac4ff43afa5fc29db0bf1fb894
Bartosz-Kacprzak <s10731bk@ms.wwsi.edu.pl>
2025-03-11 22:47:22 +0100

    Operatory_zad2

--- jipp/jipp/Program.cs
+++ jipp/jipp/Program.cs
@@ -1,13 +1,5 @@
-﻿int number = 22;
-float money = 3.2F;
-string text = "git";
-bool isLogged = true;
-char myChar = 'g';
-decimal price = 3.21M;
-Console.WriteLine(number);
-Console.WriteLine(money);
-Console.WriteLine(text);
-Console.WriteLine(isLogged);
-Console.WriteLine(myChar);
-Console.WriteLine(price);
-
+﻿string myAge = "Age: ";
+int wifeAge = 18;
+string result = myAge + wifeAge;
+Console.WriteLine(result);
+//Jeśli chcemy połączyć zmienną typu string ze zmienną typu int, to musimy skorzystać z operatora +, który pozwala na konkatenację tych dwóch zmiennych. W wyniku otrzymamy zmienną typu string, która zawiera wartość zmiennych myAge i wifeAge. 
